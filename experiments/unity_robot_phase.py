57235638df014b4e3262ca08bc11afb6e4ae5b63
Olin <olin.goog@gmail.com>
2026-03-11 13:11:09 -0400

    better VLA_STAR_PATH format

--- experiments/unity_robot_phase.py
+++ experiments/unity_robot_phase.py
@@ -5,6 +5,7 @@ import shop
 from setproctitle import setproctitle
 
 if __name__ == "__main__":
+    os.environ.get("AGENT_LABEL", sys.argv[1])
     setproctitle("vla_unity")
     vla_star = shop.instantiate_unity_robot()
     vla_star.safe_start()
