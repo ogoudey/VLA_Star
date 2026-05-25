8ba6db4f1d86461433c34b23050c52fc7ef8df3a
Olin <olin.goog@gmail.com>
2026-05-25 09:11:47 -0400

    fixed factory function for minimal VLA*

--- vla_star_configurable/vla_star_config/vla_star_configs/chatter_config.py
+++ vla_star_configurable/vla_star_config/vla_star_configs/chatter_config.py
@@ -30,7 +30,7 @@ class ChatterConfig(VLA_Star_Config):
         recorded: bool = False,
         force_uniqueness: bool=False
     ):
-        from vla_star_factory.context_engine_factories.mobile_manipulator_context_engine_factory import instantiate_mobile_manipulator_context_engine
+        from vla_star_factory.context_engine_factories.minimal_context_engine_factory import instantiate_minimal_context_engine
         _name: str = name if name or force_uniqueness else generate_unique_name()
         
         _agency_type = agency_type if agency_type else self.agency_type # should look these up
@@ -38,7 +38,7 @@ class ChatterConfig(VLA_Star_Config):
         _construction = construction if construction else self.construction_type
         _motive = motive if motive else self.motive_type
         
-        return instantiate_mobile_manipulator_context_engine(
+        return instantiate_minimal_context_engine(
             name=_name,
             agency_type=_agency_type,
             instructions=_instructions,
